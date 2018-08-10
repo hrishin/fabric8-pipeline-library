d1dfb4975fbac02ac108c69d41308ebc6fc6e46f
hriships <hrishishinde.p@gmail.com>
2018-08-10 13:12:10 +0530

    mavenIntegrationTest: allow overriding mvn commands
    This patch allows the `mavenCI{}` to over maven command in order
    to allow `mavenIntegrationTes{}` to pass specific command to execute.
    It has added a util method to retrive default test namespace.
    
    Fixes: https://github.com/openshiftio/openshift.io/issues/763
    Fixes: https://github.com/openshiftio/openshift.io/issues/3134
    Related to: https://github.com/fabric8io/fabric8-pipeline-library/pull/412

--- vars/mavenIntegrationTest.groovy
+++ vars/mavenIntegrationTest.groovy
@@ -2,22 +2,28 @@
 import io.fabric8.Utils
 
 def call(body) {
+    def utils = new Utils()
+
+    if (utils.isDisabledITests()) {
+        echo "WARNING: Integration tests DISABLED for these pipelines!"
+        return
+    }
+
     // evaluate the body block, and collect configuration into the object
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
 
-    def utils = new Utils()
     def envName = config.environment
     def kubeNS = "-Dfabric8.environment=${envName}"
-    if (envName) {
-        // lets try find the actual kubernetes namespace
+
+    if (envName && !config.integrationTestCmd) {
         try {
             def ns = utils.environmentNamespace(envName)
             if (ns) {
                 kubeNS = "-Dnamespace.use.existing=${ns}"
-                echo "Running the integration tests in the namespace: ${ns}"
+                echo "Running the integration tests in namespace : ${ns}"
             }
         } catch (e) {
             echo "ERROR: failed to find the environment namespace for ${envName} due to ${e}"
@@ -25,12 +31,8 @@ def call(body) {
         }
     }
 
-    if (utils.isDisabledITests()) {
-        echo "WARNING: Integration tests are current DISABLED for these pipelines!"
+    def testCmd = config.integrationTestCmd ?: "mvn org.apache.maven.plugins:maven-failsafe-plugin:integration-test -P openshift-it ${kubeNS} -Dit.test=${config.itestPattern} -DfailIfNoTests=${config.failIfNoTests} org.apache.maven.plugins:maven-failsafe-plugin:verify"
+    sh testCmd
+    junitResults(body);
 
-    } else {
-        sh "mvn org.apache.maven.plugins:maven-failsafe-plugin:integration-test ${kubeNS} -P openshift-it -Dit.test=${config.itestPattern} -DfailIfNoTests=${config.failIfNoTests} org.apache.maven.plugins:maven-failsafe-plugin:verify"
-
-        junitResults(body);
-    }
 }
